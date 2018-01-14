c3a3a9207e2df6a1d330e1ac79fe26daf349d39b
Johan Burke <jburke4126@gmail.com>
2018-01-14 18:00:01 -0500

    Setup OpenGL context creation and GLEW initialization.

--- components/PenroseEngine/src/Penrose_Application.cpp
+++ components/PenroseEngine/src/Penrose_Application.cpp
@@ -5,27 +5,40 @@ using namespace Penrose;
 
 bool Application::init(const std::string &title) {
     quit = false;
+    std::cout << "Attempting to intialize SDL video." << std::endl;
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "SDL_Init error: " << SDL_GetError() << std::endl;
         return false;
     }
+    std::cout << "Successfully initialized SDL video." << std::endl;
     window = std::make_unique<Window>();
+
+    std::cout << "Attempting to create Window." << std::endl;
+    if (!window->init()) {
+        std::cerr << "Error creating window." << std::endl;
+        return false;
+    }
+    std::cout << "Window created successfully." << std::endl;
+
     window->set_window_title(title);
     return true;
 }
 
 void Application::run() {
 
+    std::cout << "Starting application run loop." << std::endl;
     while (!quit) {
         handle_events();
     }
 
+    std::cout << "Ending application run loop." << std::endl;
 }
 
 void Application::handle_events() {
     SDL_Event e;
     while (SDL_PollEvent(&e)) {
         if (e.type == SDL_QUIT) {
+            std::cout << "Received SDL quit event." << std::endl;
             quit = true;
         }
     }
--- components/PenroseEngine/src/Penrose_Window.cpp
+++ components/PenroseEngine/src/Penrose_Window.cpp
@@ -1,17 +1,59 @@
 #include "Penrose_Window.hpp"
 
+#include <iostream>
+
 using namespace Penrose;
 
 Window::Window() : Window(1024, 768) {
 }
 
-Window::Window(int width, int height) {
-    win = SDL_CreateWindow("Penrose_Window", 0, 0, width, height, SDL_WINDOW_SHOWN);
+Window::Window(int width, int height)
+    : width(width), height(height) {
 
 }
 
 Window::~Window() {
     SDL_DestroyWindow(win);
+}
+
+bool Window::init() {
+    std::cout << "Attempting to set SDL gl attributes." << std::endl;
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    std::cout << "SDL gl attributes set successfully." << std::endl;
+
+    std::cout << "Attempting to create SDL gl window." << std::endl;
+    win = SDL_CreateWindow("Penrose_Window", 0, 0, width, height,
+            SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    if (!win) {
+        std::cerr << "Error creating window: " << SDL_GetError() << std::endl;
+        return false;
+    }
+    std::cout << "SDL gl window created successfully." << std::endl;
+
+    return init_gl();
+}
+
+bool Window::init_gl() {
+    std::cout << "Attempting to create SDL gl context." << std::endl;
+    gl_context = SDL_GL_CreateContext(win);
+    if (!gl_context) {
+        std::cerr << "Error creating opengl context: " << SDL_GetError() << std::endl;
+        return false;
+    }
+    std::cout << "SDL gl context created successfully." << std::endl;
+
+    std::cout << "Attempting to initialize GLEW." << std::endl;
+    glewExperimental = GL_TRUE;
+    GLenum glew_error = glewInit();
+
+    if (glew_error != GLEW_OK) {
+        std::cerr << "Error initializing GLEW: " << glewGetErrorString(glew_error) << std::endl;
+        return false;
+    }
+    std::cout << "GLEW initialized successfully." << std::endl;
+
+    return true;
 }
 
 std::string Window::get_window_title() const {
