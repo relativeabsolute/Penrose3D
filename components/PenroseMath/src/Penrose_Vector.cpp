c98c38e49da849c3e57f3a41ada7f311e2370ed7
Johan Burke <jburke4126@gmail.com>
2018-01-14 22:32:23 -0500

    Implemented vector functionality and partial unit testing.

---
+++ components/PenroseMath/src/Penrose_Vector.cpp
@@ -0,0 +1,13 @@
+#include <Penrose_Vector.hpp>
+#include <Penrose_Vector_impl.hpp>
+
+namespace Penrose::Math {
+    template class Vector<float, 2ul>;
+    template class Vector<float, 3ul>;
+    template class Vector<float, 4ul>;
+
+    template class Vector<double, 2ul>;
+    template class Vector<double, 3ul>;
+    template class Vector<double, 4ul>;
+
+}
